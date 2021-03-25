98a7e806d01976acf61db2b30011e289e6850b9d
Roman Yarmolenko <devxypro@gmail.com>
2021-03-25 14:04:56 +0200

    Add enterprise updater

---
+++ update_enterprises.py
@@ -0,0 +1,21 @@
+import os
+
+from pymongo import MongoClient
+
+from sync_interface import SyncInterface
+
+
+class EnterpriseUpdater:
+    def __init__(self):
+        self.mongo_client = MongoClient(os.environ['MONGO_HOST'], int(os.environ['MONGO_PORT']))
+        self.db = self.mongo_client.feedback_monitoring
+        self.collection = self.db.enterprises
+
+    def update(self):
+        for slug, enterprise in SyncInterface.get_config().items():
+            doc = {'enterprise_id': enterprise['enterprise_id'], 'slug': slug}
+            self.collection.update_one({"enterprise_id": doc["enterprise_id"]}, {"$set": doc}, upsert=True)
+
+
+u = EnterpriseUpdater()
+u.update()
