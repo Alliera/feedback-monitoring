6efb24aacbd42da1607336afbf8d5390880d370c
Roman Yarmolenko <devxypro@gmail.com>
2021-04-15 16:59:33 +0300

    Scheduler cahnge time

--- scheduler.py
+++ scheduler.py
@@ -4,18 +4,21 @@ from datetime import timedelta
 from sync_interface import SyncInterface
 import export_bot
 import import_bot
+from dotenv import load_dotenv
+
+load_dotenv()
 
 tl = Timeloop()
 export_stat = export_bot.ExportBot()
 import_stat = import_bot.ImportBot()
 
 
-@tl.job(interval=timedelta(hours=1))
+@tl.job(interval=timedelta(seconds=1))
 def _export():
     run_command(export_stat)
 
 
-@tl.job(interval=timedelta(hours=1))
+@tl.job(interval=timedelta(seconds=1))
 def _import():
     run_command(import_stat)
 
@@ -27,7 +30,7 @@ def run_command(bot):
         try:
             bot.sync()
         except Exception as e:
-            print("ERROR---------->" + str(e))
+            print("ERROR----------> for " + bot.get_name() + ": " + str(e))
         print("Sync " + bot.get_name() + " complete")
         print("Current time : {}".format(time.ctime()))
 
